bc028f4ff9472e64bfc99dae012f24253440d5f4
John Nicholas <themrtortoise@gmail.com>
2017-02-20 23:25:43 +0000

    following elm-tutorial

--- take3/Hello.elm
+++ take3/Hello.elm
@@ -2,7 +2,27 @@ module Hello exposing (..)
 
 import Html exposing (text)
 
+
 main : Html.Html msg
 main =
   text "Hello"
-  
+
+
+type OtherAnswer = DontKnow | Perhaps | Undecided
+
+type Answer = Yes | No | Other OtherAnswer
+
+respond : Answer -> String
+respond answer =
+  case answer of
+     Yes -> "Yes"
+     (Other DontKnow) -> "dunno"
+     _ -> "somethingelse"
+
+type alias Message a =
+  {code : String,
+  body : a
+}
+
+readMessage : Message String -> String
+readMessage message =

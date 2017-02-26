7bacf96f364650c57fad0c48e96e78e3b58d7fa6
John Nicholas <themrtortoise@gmail.com>
2017-02-26 23:02:25 +0000

    routing and navigation in place

--- planning/src/Main.elm
+++ planning/src/Main.elm
@@ -1,16 +1,21 @@
 module Main exposing (..)
 
-import Html exposing (program)
 import Msgs exposing (Msg)
 import Models exposing (Model, initialModel)
 import Update exposing (update)
 import View exposing (view)
 import Commands exposing (fetchPlayers)
+import Navigation exposing (Location)
+import Routing
 
 
-init : ( Model, Cmd Msg )
-init =
-    ( initialModel, fetchPlayers )
+init : Location -> ( Model, Cmd Msg )
+init location =
+    let
+        currentRoute =
+            Routing.parseLocation location
+    in
+        ( initialModel currentRoute, fetchPlayers )
 
 
 
@@ -28,7 +33,7 @@ subscriptions model =
 
 main : Program Never Model Msg
 main =
-    program
+    Navigation.program Msgs.OnLocationChange
         { init = init
         , view = view
         , update = update
